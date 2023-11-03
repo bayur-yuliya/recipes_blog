9dfbf9a18d2d8cfb29f2f5e08cec08b32bf3099b
Yuliya Bayur <juliy14497@gmail.com>
2023-11-03 22:13:43 +0200

    Added views, urls, templates

--- recipes_blog/recipes_app/views.py
+++ recipes_blog/recipes_app/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+menu = [
+        {"title": "Добавить статью", 'url_name': 'add_page'},
+        {"title": "Обратная связь", 'url_name': 'contact'},
+        {"title": "Войти", 'url_name': 'login'},
+        ]
+
+
+def index(request):
+
+    data = {
+        "title": "Главная страница",
+        "menu": menu,
+    }
+    return render(request, "recipes_app/index.html", context=data)
+
+
+def about(request):
+    return render(request, "recipes_app/about.html", {"title": "О нас", 'menu': menu})
+
+
+def add_page(request):
+    return HttpResponse('Добавить статью')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Войти')
+
+
+def post(request, post_id):
+    data = {'data': post_id}
+    return render(request, 'recipes_app/post.html', context=data)
+
+
+def show_categories(request, cat_id):
+    return index(request)
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена. </h1>")
