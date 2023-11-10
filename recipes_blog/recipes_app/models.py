04476208f93ec218393a84913222edff280b8d30
Yuliya Bayur <juliy14497@gmail.com>
2023-11-10 20:03:39 +0200

    Add model Recipe and migrations

--- recipes_blog/recipes_app/models.py
+++ recipes_blog/recipes_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    ingredients = models.TextField(blank=True)
+    recipe_text = models.TextField(blank=True)
+    cooking_time = models.CharField(max_length=30, blank=True, null=True)
+    quantity_of_servings = models.PositiveIntegerField(blank=True, null=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
