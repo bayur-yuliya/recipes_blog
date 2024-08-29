ac9f22ad0d239d7157df345a8dad7f9096ea0d52
Baiur Yuliia <juliy14497@gmail.com>
2024-08-29 22:57:10 +0300

    Add management command by create recipes

--- recipes_app/management/commands/create_recipes.py
+++ recipes_app/management/commands/create_recipes.py
@@ -7,19 +7,42 @@ class Command(BaseCommand):
     help = """the command generates a recipes"""
 
     def handle(self, *args, **options):
-        Recipe.objects.create(
-            title="Шарлотка",
-            ingredients="Яблоки, Мука, яица, сахар",
-            recipe_text="Приготовьте все ингредиенты для шарлотки. "
-            "Включите разогреваться духовку до 180 градусов. "
-            "Начните приготовление пирога с начинки. Возьмите 3 яблока, помойте, "
-            "очистите от кожуры, удалите семена и плодоножку. "
-            "Нарежьте сначала на дольки, а потом на кусочки одинакового размера. "
-            "По желанию можете не срезать кожуру.",
-            cooking_time="30 мин",
-            quantity_of_servings=4,
-            is_published=1,
-            slug="sharlotka",
+        recipes = (
+            (
+                "Шарлотка",
+                "4 яблока,160 г муки, 20 г сливочного масла, 4 яйца, 150 г сахара",
+                "45 мин",
+                4,
+                1,
+                "sharlotka1",
+            ),
+            (
+                "Кабачки по-корейски",
+                "1 средний кабачок, 1 средняя морковь, 1 ч. л. соли, 1 ч. л. сахар",
+                "15 мин",
+                4,
+                1,
+                "kabachki-po-korejski",
+            ),
+            (
+                "Абрикосовое варенье",
+                "1 кг абрикосов, 500 г сахара, 100 г миндальных хлопьев",
+                "30 мин",
+                1,
+                1,
+                "abrikosovoe-varene-s-mindalem",
+            ),
         )
+
+        for recipe in recipes:
+            Recipe.objects.create(
+                title=recipe[0],
+                ingredients=recipe[1],
+                recipe_text="recipe_text",
+                cooking_time=recipe[2],
+                quantity_of_servings=recipe[3],
+                is_published=recipe[4],
+                slug=recipe[5],
+            )
 
         self.stdout.write(self.style.SUCCESS("Все прошло успешно!"))
