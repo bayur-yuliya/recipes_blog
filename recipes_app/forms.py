d7bf76e59f6e5145549cfe1dd1ecbbea2ee7cd2f
Baiur Yuliia <juliy14497@gmail.com>
2024-08-31 23:53:09 +0300

    Github actions black

--- recipes_app/forms.py
+++ recipes_app/forms.py
@@ -4,15 +4,21 @@ from .models import Recipe
 
 
 class RecipeForm(forms.ModelForm):
+    title = (forms.CharField(max_length=100),)
+    ingredients = (forms.CharField(widget=forms.Textarea),)
+    recipe_text = (forms.CharField(widget=forms.Textarea),)
+    cooking_time = forms.CharField(max_length=100)
+    quantity_of_servings = forms.IntegerField()
+    is_published = forms.BooleanField()
+    slug = forms.SlugField()
 
-        title = forms.CharField(max_length=100),
-        ingredients = forms.CharField(widget=forms.Textarea),
-        recipe_text = forms.CharField(widget=forms.Textarea),
-        cooking_time = forms.CharField(max_length=100)
-        quantity_of_servings = forms.IntegerField()
-        is_published = forms.BooleanField()
-        slug = forms.SlugField()
-
-        class Meta:
-            model = Recipe
-            fields = ("title", "ingredients", "recipe_text", "cooking_time", "quantity_of_servings", "slug")
+    class Meta:
+        model = Recipe
+        fields = (
+            "title",
+            "ingredients",
+            "recipe_text",
+            "cooking_time",
+            "quantity_of_servings",
+            "slug",
+        )
