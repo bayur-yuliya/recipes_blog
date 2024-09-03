776d656912728c56ce5e4c4679160c1fe03ba0b8
Baiur Yuliia <juliy14497@gmail.com>
2024-09-03 23:10:34 +0300

    Add registration

--- recipes_app/forms.py
+++ recipes_app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Recipe
 
@@ -22,3 +24,21 @@ class RecipeForm(forms.ModelForm):
             "quantity_of_servings",
             "slug",
         )
+
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label="Username", required=True)
+    email = forms.EmailField(label="Email address", required=True)
+    password1 = forms.CharField(label="Password", required=True)
+    password2 = forms.CharField(label="Password confirmation", required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- recipes_app/views.py
+++ recipes_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound
 
+from .forms import RegisterUserForm
 from .models import Recipe
 
 
@@ -26,7 +27,14 @@ def contact(request):
 
 
 def login(request):
-    return HttpResponse("Войти")
+    if request.method == "GET":
+        form = RegisterUserForm()
+        return render(request, "register/login.html", {"form": form})
+
+    form = RegisterUserForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return render(request, "register/login.html", {"form": form})
 
 
 def post(request, recipe_slug):
