d61dc5b640c51587d4f6674cd65bcdf2ff43b35a
Baiur Yuliia <juliy14497@gmail.com>
2024-10-09 23:16:12 +0300

    Add css to index.html and post.html, add categities

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -10,12 +10,14 @@ from .models import Recipe, Category
 
 def index(request):
     recipes = Recipe.published.filter(is_published=True)
-    paginator = Paginator(recipes, 10)
+    сategory = Category.objects.all()
+    paginator = Paginator(recipes, 3)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     data = {
         "title": "Главная страница",
         "page_obj": page_obj,
+        'сategory': сategory,
     }
     return render(request, "recipes_app/index.html", context=data)
 
