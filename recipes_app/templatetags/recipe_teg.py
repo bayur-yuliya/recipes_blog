b1fd5d4debaff4a02bafb7ab3294be77b699374b
Baiur Yuliia <juliy14497@gmail.com>
2024-10-18 23:23:59 +0300

    Add inclusion_tag

--- recipes_app/templatetags/recipe_teg.py
+++ recipes_app/templatetags/recipe_teg.py
@@ -7,3 +7,9 @@ register = template.Library()
 @register.simple_tag()
 def get_categories():
     return views.cats_db
+
+
+@register.inclusion_tag("recipes_app/list_categories.html")
+def show_categories():
+    cats = views.cats_db
+    return {"cats": cats}
